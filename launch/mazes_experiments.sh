80d7083f36db550a52d603a50128c8bf38af3f34
mcleish7 <smcleish7@hotmail.co.uk>
2023-01-05 11:25:41 +0000

    initial commit

--- launch/mazes_experiments.sh
+++ launch/mazes_experiments.sh
@@ -1,4 +1,4 @@
-python train_model.py problem.hyp.alpha=0.00 problem/model=dt_net_2d problem=mazes name=mazes_ablation
+python3.9 train_model.py problem.hyp.alpha=0.00 problem/model=dt_net_2d problem=mazes name=mazes_ablation
 python train_model.py problem.hyp.alpha=0.01 problem/model=dt_net_2d problem=mazes name=mazes_ablation
 python train_model.py problem.hyp.alpha=0.50 problem/model=dt_net_2d problem=mazes name=mazes_ablation
 python train_model.py problem.hyp.alpha=1.00 problem/model=dt_net_2d problem=mazes name=mazes_ablation
